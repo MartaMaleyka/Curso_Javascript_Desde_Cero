78874e51e2d077d208882db8d8766cc27dc15059
MartaMaleyka <martamagallon1994@gmail.com>
2024-01-23 20:56:10 -0500

    Clase de operadores logicos

---
+++ clase_operadores_logicos.js
@@ -0,0 +1,18 @@
+// Operador AND (&&): Devuelve true si ambas condiciones son verdaderas.
+var x = 5;
+var y = 10;
+if (x > 0 && y > 0) {
+    console.log("Ambas condiciones son verdaderas");
+}
+
+// Operador OR (||): Devuelve true si al menos una de las condiciones es verdadera.
+var edad = 25;
+if (edad < 18 || edad > 65) {
+    console.log("La persona es menor de 18 años o mayor de 65 años");
+}
+
+// Operador NOT (!): Devuelve true si la condición es falsa y viceversa.
+var esFalso = false;
+if (!esFalso) {
+    console.log("La expresión es verdadera");
+}
